d87cdcd4fd12a699dc7ec6de2443436f29fcc406
guy567856 <164834249+guy567856@users.noreply.github.com>
2026-01-12 17:08:05 -0500

    1.5.5 changes

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -1,7 +1,7 @@
 NTI = {}
 NTI.Name = "Infections"
-NTI.Version = "1.5.3"
-NTI.VersionNum = 10500003
+NTI.Version = "1.5.5"
+NTI.VersionNum = 10500005
 NTI.Path=table.pack(...)[1]
 Timer.Wait(function() if NTC ~= nil and NTC.RegisterExpansion ~= nil then NTC.RegisterExpansion(NTI) end end,1)
 
